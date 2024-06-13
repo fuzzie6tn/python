1cc43e26b2afed094b15757fcf755faf1cb39a78
fazila <fazilaroushanbeg@gmail.com>
2024-06-14 01:35:44 +0500

    push

--- calculator.py
+++ calculator.py
@@ -1,6 +1,13 @@
-x = float(input("what is x? "));
-y = float(input("what is y? "));
-# print(int(x)+int(y));  #dont use only x and y it will concatinate it  
-z =  x/y
-# print(f'{z:.2f}') #this is going to round off by 2 digit 
-print(f'{z}')
+# x = float(input("what is x? "));
+# y = float(input("what is y? "));
+# # print(int(x)+int(y));  #dont use only x and y it will concatinate it  
+# z =  x/y
+# # print(f'{z:.2f}') #this is going to round off by 2 digit 
+# print(f'{z}')
+def main():
+    x = int(input('value of x? '))
+    print(x, ' square is ', square(x))
+def square(n):
+    return n**2
+main()    
+
--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-name = input("what's your name").capitalize().strip().title()
-# name = name.strip()
-# name = name.capitalize()
-print(f"Hello , {name}") # we can also use + or , 
+# name = input("what's your name").capitalize().strip().title()
+# # name = name.strip()
+# # name = name.capitalize()
+# print(f"Hello , {name}") # we can also use + or , 
+def hello(to):
+    print('hello,', to)
+name = input("what is your name? ")
+hello(name) 
