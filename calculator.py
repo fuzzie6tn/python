a34c319cfd7f36ffe9247eb8988eba454426f6af
fazila <fazilaroushanbeg@gmail.com>
2024-06-13 17:38:12 +0500

    push

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
-x = 1;
-y = 2;
-z = x+y;
-print(z);
+x = float(input("what is x? "));
+y = float(input("what is y? "));
+# print(int(x)+int(y));  #dont use only x and y it will concatinate it  
+z =  x/y
+# print(f'{z:.2f}') #this is going to round off by 2 digit 
+print(f'{z}')
