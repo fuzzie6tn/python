83f1cd73e52505dca123259728f46ee15bdabe3f
fazila <fazilaroushanbeg@gmail.com>
2024-06-14 15:57:56 +0500

    push

---
+++ score.py
@@ -0,0 +1,11 @@
+score = int (input('enter your score: '))
+if 90<=score and score<=100:
+    print('grade: A')
+elif 80<=score and score<90:
+    print('grade: B')
+elif 70<=score and score <80:
+    print('grade: C')
+elif 60<=score and score<70:
+    print('grade: D')
+else:
+    print('grade: F')    
