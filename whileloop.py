db8891d3c416cf207d3caf6854d7353a816b1414
fazila <fazilaroushanbeg@gmail.com>
2024-06-15 16:05:45 +0500

    psh

--- whileloop.py
+++ whileloop.py
@@ -3,4 +3,42 @@
 #     print('meow')
 #     i=i-1
 
-print("meow\n"*3, end="")
+# print("meow\n"*3, end="")
+
+# while True:
+#     n = int(input("what is n? "))
+#     if n>0:
+#         break
+
+# for _ in range(n):
+#         print('meow')
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("what the number? "))
+        if n>0:
+            break
+    return n 
+def meow(n):
+    for _ in range(n):
+        print('meow')    
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
