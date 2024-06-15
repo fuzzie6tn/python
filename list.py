baff6512ddc250f275e521efdb9e94bd8ed9e08c
fazila <fazilaroushanbeg@gmail.com>
2024-06-15 17:36:15 +0500

    psh

---
+++ list.py
@@ -0,0 +1,11 @@
+# students = ['fazi', 'muni', 'malika']
+# for i in range(len(students)):
+#     print(students[i])
+
+# dict - associateing something with something.
+students = { 'fazi': 'house',
+            'muni':'hostel',
+            'malika': 'hostel'
+}
+for student in students:
+    print(student, students[student], sep=' , ')
