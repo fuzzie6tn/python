13e469ec1e96aa3795fa25c2e974be189d52a5ef
fazila <fazilaroushanbeg@gmail.com>
2024-06-17 03:31:24 +0500

    psh

--- exceptions.py
+++ exceptions.py
@@ -3,10 +3,40 @@
 #     print(f'x is {x}')
 
 # except ValueError:
+#     print('x is not an integer')  
+# ----------  
+# try:
+#     x = int (input('what is x?'))
+# except ValueError:
 #     print('x is not an integer')    
-try:
-    x = int(input('what is x?'))
-except:
-    print('x is not an interger')
-else:
-    print(f'x is {x}')    
+# print(f'x is {x}')
+# to solve this 
+# -------
+# try:
+#     x = int(input('what is x?'))
+# except:
+#     print('x is not an interger')
+# else:
+#     print(f'x is {x}')    
+# prompting user to enter value again and again
+# --------------
+
+# while True:
+#     try:
+#         x = int(input('what is x?'))
+
+#     except ValueError:
+#         print('x is not and integer')
+#     else:
+#         break    
+# print(f'x is {x}')        
+def main():
+    x = getint()
+    print(f'x is {x}')
+def getint():
+    while True:
+        try:
+            return int(input('what is x?'))
+        except ValueError:
+            print('x is not an integer')
+main()
