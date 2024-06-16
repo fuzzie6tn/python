80bd92f636c21846d07b089699d2b9b098ac7084
fazila <fazilaroushanbeg@gmail.com>
2024-06-17 02:31:05 +0500

    psh

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,12 @@
-try:
-    x = int (input('what is x?'))
-    print(f'x is {x}')
+# try:
+#     x = int (input('what is x?'))
+#     print(f'x is {x}')
 
-except ValueError:
-    print('x is not an integer')    
+# except ValueError:
+#     print('x is not an integer')    
+try:
+    x = int(input('what is x?'))
+except:
+    print('x is not an interger')
+else:
+    print(f'x is {x}')    
