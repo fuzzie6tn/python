c9615c49206b634ce318ecfb4c584e544ff7dea8
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-21 11:34:22 +0500

    psh

---
+++ 100DaysofPython/day5/day5_project.py
@@ -0,0 +1,20 @@
+# password generator
+
+import random
+
+letters = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 'K', "L", 'M', 'N', "O", 'P', 'Q', "R", 'S', 'T', 'U', "V", "W", 'X'," Y",' Z'
+    , "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","a","v","w","x","y","z"
+
+]
+numbers = [
+    "0","1","2","3","4","5","6","7","8","9",
+]
+symbols = [
+    "!","-","+","@","$","#",")","(","%","|",
+]
+
+print("Welcome to password generator!")
+letter_ = int (input("How many letters do you want in your password? "))
+number_ = int(input("How many numbers do you want in your password? "))
+symbols_ = int(input("How many symbols? "))
