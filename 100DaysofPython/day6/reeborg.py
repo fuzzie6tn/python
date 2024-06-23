4c9b29ea80d5fb4761786e2e574ee584f443e1f5
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-23 14:22:46 +0500

    psh

---
+++ 100DaysofPython/day6/reeborg.py
@@ -0,0 +1,23 @@
+def turn_right():  
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_right() 
+    move()
+    turn_right
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+def front_is_clear():
+
+def wall_in_front():
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+            move()                   
