9140f65af2f41b0351914e62e5bd65834e103286
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-23 23:25:38 +0500

    psh

--- 100DaysofPython/day7/hangman.py
+++ 100DaysofPython/day7/hangman.py
@@ -5,13 +5,15 @@ word_list = ['ardvark', 'baboon', 'camel']
 chosen_word = random.choice(word_list)
 print(chosen_word)
 display = []
+word_length = len(chosen_word)
 for _ in chosen_word:
     display += "_"
 print (display)
 guess = input("Guess the letter: ").lower()
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right!")
-    else:
-        print("Wrong!")    
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess: 
+        display[position] = letter
+
+print(display)        
