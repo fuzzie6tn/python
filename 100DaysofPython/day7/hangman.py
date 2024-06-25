a1c1ca4858910c14930c93a34b183e84051f9f5e
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-25 16:31:13 +0500

    psh

--- 100DaysofPython/day7/hangman.py
+++ 100DaysofPython/day7/hangman.py
@@ -1,19 +1,38 @@
 import random
+import hangman_stages
+import hangman_words
 
-word_list = ['ardvark', 'baboon', 'camel']
-
-chosen_word = random.choice(word_list)
+print(hangman_stages)
+chosen_word = random.choice(hangman_words)
+lives = 6
 print(chosen_word)
+
 display = []
+
 word_length = len(chosen_word)
+
 for _ in chosen_word:
     display += "_"
 print (display)
-guess = input("Guess the letter: ").lower()
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess: 
-        display[position] = letter
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess the letter: ").lower()
 
-print(display)        
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position : {position}\nCurrent letter: {letter}\nGuessed letter: {guess}")
+        if letter == guess: 
+            display[position] = letter
+    print(display)        
+
+    if guess is not chosen_word:
+        lives-=1
+        if lives==0:
+            end_of_game = True
+            print("You lose!")
+    if "_"  not in display:
+        end_of_game = True
+        print("You Win!")
+
+    print(hangman_stages[lives])
