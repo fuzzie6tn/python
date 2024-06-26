af4948652fb2566e5a5f0d10216fd2d742a1ec6d
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-27 00:34:20 +0500

    psh

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -1,4 +1,4 @@
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 direction = input("type 'encode' to encrypt, and 'decode' to decrypt:\n")
 text = input("type your message: \n").lower()
 shift = int(input("type the shift number:\n"))
