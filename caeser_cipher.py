f462ce5f230bb6269e6af5582da2064b93976115
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-27 01:02:55 +0500

    psh

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -1,8 +1,8 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 direction = input("type 'encode' to encrypt, and 'decode' to decrypt:\n")
+
 text = input("type your message: \n").lower()
 shift = int(input("type the shift number:\n"))
-
 def encrypt(plain_text, shift_num):
     cipher_text = ""
     for letter in plain_text:
@@ -11,5 +11,19 @@ def encrypt(plain_text, shift_num):
         new_letter = alphabet[new_postition]
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
+def decrypt(cipher_text, shift_num):
+    decipher_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_num
+        new_letter = alphabet[new_position]
+        decipher_text += new_letter
+    print(f"The decoded text is {decipher_text}")
 
-encrypt(plain_text = text, shift_num = shift)
+if direction == "encode":
+    encrypt(plain_text = text, shift_num = shift)
+elif direction == "decode":
+    
+    decrypt(cipher_text = text, shift_num = shift)    
+else:
+    print("You typed a wrong option")    
