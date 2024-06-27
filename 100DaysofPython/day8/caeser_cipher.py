9c67d5ab59343bfe35df3d8f9d1348a30f2507ae
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-27 15:21:47 +0500

    psh

--- 100DaysofPython/day8/caeser_cipher.py
+++ 100DaysofPython/day8/caeser_cipher.py
@@ -1,16 +1,28 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-direction = input("type 'encode' to encrypt, and 'decode' to decrypt:\n")
-
-text = input("type your message: \n").lower()
-shift = int(input("type the shift number:\n"))
-
-def caeser(text, shift, direction):
+def caeser(start_text, shift_num, cipher_direction):
     end_text = ""
-    if direction == "decode":
-        shift *= -1
-    for letter in text:
-        position = alphabet.index(letter)
-        new_position = position + shift
-        end_text += alphabet[new_position]
-    print(f"The {direction} text  is {end_text}")        
-caeser(text=text, shift=shift,direction=direction)    
+    if cipher_direction == "decode":
+        shift_num *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_num
+            end_text += alphabet[new_position]
+        else:
+            end_text += char    
+    print(f"The {cipher_direction} text  is {end_text}")        
+
+from art import logo
+print(logo)
+
+should_continue = True
+while should_continue:
+    direction = input("type 'encode' to encrypt, and 'decode' to decrypt:\n")
+    text = input("type your message: \n").lower()
+    shift = int(input("type the shift number:\n"))
+    shift = shift % 26
+    caeser(start_text = text, shift_num = shift,cipher_direction = direction)     
+    result = input("type 'y' to continue or 'n' to exit\n")
+    if result == 'n':
+        should_continue = False
+        print("good bye\n")
