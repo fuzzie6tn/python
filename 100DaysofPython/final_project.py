57dc5deabd9fcf60a7b98b330d1fca1374898409
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-29 21:16:41 +0500

    psh

--- 100DaysofPython/final_project.py
+++ 100DaysofPython/final_project.py
@@ -1,42 +1,55 @@
 import random 
+#I HAVE DONE THIS BY MYSELF!!!!
+# NUMBER_TO_GUESS = random.choice(1,100)
+# NUMBER_TO_GUESS = 50
 
-# NUMBER_TO_GUESS = random.choice(0,100)
-NUMBER_TO_GUESS = 50
+from logo import logo
+print(logo)
+print("Welcome to the Number guessing game\nI'm thinking of a number between 1 and 100\n")
 
-print("Welcome to the Number guessing game\nI'm thinking a number between 1 and 100\n")
-level = input("Choose a difficulty: easy or hard?\n")
-    
-if level == "easy":
-    print("You have 10 attempts remaining to guess the number.")
-    attempts = 10
-    while attempts < 10:
-        guess = int(input("Guess the number: "))
-        if guess == NUMBER_TO_GUESS:
-            print("You got it.")
-        elif guess > NUMBER_TO_GUESS:    
-            print("Too high")
-        else:
-            print("Too low") 
+NUMBER_TO_GUESS = random.choice(range(1, 101))
 
-    attempts -= 1
-    print(f"{attempts} left")
-    print("You have used all your attempts")
+# for num in range(1, 101):
+#     print(num)
 
-elif level == "hard":
-    print("You have 5 attempts remaining to guess the number.")
-    attempts = 5
-    while not attempts:
-        guess = input("Guess the number: ")
-        if guess == NUMBER_TO_GUESS:
-            print("You got it.")
-        elif guess > NUMBER_TO_GUESS:    
-            print("Too high")
-        else:
-            print("Too low") 
+def gues_the_num():
+    level = input("Choose a difficulty: easy or hard?\n")
 
-        attempts -= 1
-        print(f"{attempts} left")
-    print("You have used all your attempts")
-        
-else:
-    print(f"{level} is not in this game")    
+    if level == "easy":
+        print("You have 10 attempts remaining to guess the number.")
+        attempts = 10
+        while attempts:
+            guess = int(input("Guess the number: "))
+            if guess == NUMBER_TO_GUESS:
+                print("You got it.")
+                break
+            elif guess > NUMBER_TO_GUESS:    
+                print("Too high")
+            else:
+                print("Too low") 
+
+            attempts -= 1
+            print(f"You have {attempts} attempts left")
+        print("You have used all your attempts")
+
+    elif level == "hard":
+        print("You have 10 attempts remaining to guess the number.")
+        attempts = 5
+        while attempts:
+            guess = int(input("Guess the number: "))
+            if guess == NUMBER_TO_GUESS:
+                print("You got it.")
+                break
+            elif guess > NUMBER_TO_GUESS:    
+                print("Too high")
+            else:
+                print("Too low") 
+
+            attempts -= 1
+            print(f"You have {attempts} attempts left")
+        print("BYE")
+            
+    else:
+        print(f"{level} is not in this game")    
+
+gues_the_num()        
