00ce591bee28be4270e0c4590dbbda29b3182d65
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-06-30 00:19:26 +0500

    psh

---
+++ 100DaysofPython/debugging.py
@@ -0,0 +1,6 @@
+def func():
+    for i in range(1,21):
+        if i==20:
+            print("You got it!")
+
+func()            
---
+++ 100DaysofPython/reprouduce_bug.py
@@ -0,0 +1,5 @@
+from random import randint
+
+dice_imgs = ["1","2","3","4","5","6"]
+dice_num = randint(0,5)
+print(dice_imgs[dice_num])
