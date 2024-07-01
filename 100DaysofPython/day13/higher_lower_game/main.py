df8851a7155fe0da3e9bc08bdc45ba22db2255d2
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-01 16:21:39 +0500

    psh

--- 100DaysofPython/day13/higher_lower_game/main.py
+++ 100DaysofPython/day13/higher_lower_game/main.py
@@ -1,55 +1,61 @@
-# import random
-# from art import logo, vs
-# from gamedata import data
-
-# print(logo)
-
-# # Ensure data has at least two items
-# if len(data) < 2:
-#     raise ValueError("Not enough data to compare")
-
-# # Randomly select two different items
-# item_a, item_b = random.sample(data, 2)
-
-# # Print comparison for item A
-# print(f"Compare A: {item_a['name']} a {item_a['description']} from {item_a['country']}")
-
-# print(vs)
-
-# # Print comparison for item B
-# print(f"Compare B: {item_b['name']} a {item_b['description']} from {item_b['country']}")
-
-# # Ask the user for input
-# answer = input("Who has more followers? Type 'A' or 'B': ")
-# ----------------------------------------------------------------------------------------------------
-
 #display art
-from art import logo
+from art import logo, vs
 from gamedata import data
 import random
-
-print(logo)
-# we made this function more reusable for both accounts
-def format_data(account):
-    '''Format the account data into printable format'''
-    account_name = account["name"]
-    account_desc = account["description"]
-    account_country = account["country"]
-    print(f"{account_name}, a {account_desc} from {account_country}")
-
-
-# Generate a random account from the game data
-accountA = random.choice(data)
+import os
+score = 0
+game_should_continue =True
 accountB = random.choice(data)
-if accountA == accountB:
-    accountB = random.choice(data) #regenerated B if they both are same. 
 
 
+def clear():
+    """Clear the console for a new game"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+def format_data(account):
+        '''Format the account data into printable format'''
+        account_name = account["name"]
+        account_desc = account["description"]
+        account_country = account["country"]
+        return(f"{account_name}, a {account_desc} from {account_country}")
+def check_answer(guess, a_followers, b_followers):
+    '''     take the user guess and follower counts and returns if they got it right   '''
+
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+while game_should_continue:
+    print(logo)
+    # Generate a random account from the game data
+    accountA = accountB
+    accountB = random.choice(data)
+    while accountA == accountB:  
+        accountB = random.choice(data) #regenerated B if they both are same. 
+
+    print(f"Compare A: {format_data(accountA)}.")
+    print(vs)
+    print(f"Against B: {format_data(accountB)}.")
+
+    # Ask user for a guess for who has more follwers
+    guess = input("Who has more followers has  more followers A or B? ").lower()
 
 
+    #Check if the user is correct
+    #Get the follower count of each account
+    a_follower_count = accountA["follower_count"]
+    b_follower_count = accountB["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-# Ask user for a guess
+    clear()
+    #give user feedback 
 
-#Check if the user is correct
+    if is_correct:
+        score += 1
+        print(f"You are right! Current score {score}")
+    else :
+        game_should_continue = False
+        print(f"sorry, thats wrong. Final Score {score}") 
+
+
 
 
