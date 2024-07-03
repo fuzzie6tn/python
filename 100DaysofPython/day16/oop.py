e06f1edace87622c4e6b0a60759910bac13e6803
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-04 00:38:34 +0500

    psh

---
+++ 100DaysofPython/day16/oop.py
@@ -0,0 +1,17 @@
+# import another_module
+# print(another_module.another_variable)
+import prettytable
+from turtle import Turtle, Screen
+timmy = Turtle() #new object from turtle class  timmy is the object here
+print(timmy) # object printed
+timmy.shape('turtle')
+timmy.color('blue')
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+my_Screen  = Screen()
+print(my_Screen.exitonclick())
