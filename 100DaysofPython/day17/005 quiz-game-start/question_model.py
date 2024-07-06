79202e061102d4abe8e0faee9f6bcc1926baa8d8
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-07 02:08:32 +0500

    psh

--- 100DaysofPython/day17/005 quiz-game-start/question_model.py
+++ 100DaysofPython/day17/005 quiz-game-start/question_model.py
@@ -3,7 +3,3 @@ class Question:
         self.text = question_text
         self.answer = correct_answer
 
-new_q = Question("What is this?", "this is something")
-print(new_q.text)
-print(new_q.answer)
-
---
+++ 100DaysofPython/day17/005 quiz-game-start/quiz_brain.py
@@ -0,0 +1,30 @@
+''' Asking the user the next questions
+    checking if the answers were right
+    checking if we are the end of the quiz
+'''
+
+
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q. {self.question_number}: {current_question} (True/False)")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print(f"You're right! The correct answer is {correct_answer}")
+            print(f"Your current score is: {self.score/len(self.question_list)}")
+        else:
+            print(f"You're wrong! The correct answer is {correct_answer}")
+        print(f"Your current score is :{self.score/self.question_list}")
