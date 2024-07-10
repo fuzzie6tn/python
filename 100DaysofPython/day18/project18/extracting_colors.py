25e317c8c7e61c0b6601cc4dda6be24944edb632
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-10 15:34:06 +0500

    psh

--- 100DaysofPython/day18/project18/extracting_colors.py
+++ 100DaysofPython/day18/project18/extracting_colors.py
@@ -1,5 +1,4 @@
-import turtle as t
-import random
+"""Extracting colors. A tuple with R value G value and B value"""
 import colorgram
 
 '''Extracting colors from the picture hirst'''
@@ -18,3 +17,4 @@ for color in colors:
     rgb_colors.append(new_color)
 
 print(rgb_colors)
+color_list = [(1, 9, 29), (121, 96, 42), (238, 211, 72), (77, 34, 23), (221, 80, 59), (225, 117, 100), (92, 1, 21), (178, 140, 171), (151, 92, 116), (35, 90, 26), (8, 154, 73), (204, 64, 92), (220, 178, 219), (167, 129, 76), (1, 63, 146), (4, 220, 218), (3, 80, 30), (80, 135, 178), (78, 115, 146), (131, 156, 178), (122, 186, 167), (122, 11, 32), (10, 216, 222), (137, 221, 208), (245, 202, 7), (229, 174, 166)]
