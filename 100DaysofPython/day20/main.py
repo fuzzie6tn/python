0f9df58f795537392d4fb5bd9fd1e0268d876c51
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-14 02:17:19 +0500

    psh

--- 100DaysofPython/day20/main.py
+++ 100DaysofPython/day20/main.py
@@ -1,34 +1,20 @@
 from turtle import Turtle, Screen
 import time
-
+from snake import Snake
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')  # background color
 screen.title("My snake game")
 screen.tracer(0)
 
-# body of snake
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for positions in starting_position:
-    new_segment = Turtle("square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.goto(positions)
-    segments.append(new_segment)
-
+snake = Snake()
 
 # move the snake
 game_is_on = True
 while game_is_on:
     screen.update()  # each update moves
-    time.sleep(0.2)  # 1 second delay
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    time.sleep(0.1)  # 1 second delay
 
-
+    snake.move()
 screen.exitonclick()
 
--- 100DaysofPython/day20/snake.py
+++ 100DaysofPython/day20/snake.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0), (40, 0)]
+MOVE_DISTANCE = 20
+
+
 class Snake:
     def __init__(self):
         self.segments = []
@@ -12,9 +15,10 @@ class Snake:
             new_segment.color("white")
             new_segment.goto(positions)
             self.segments.append(new_segment)
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
