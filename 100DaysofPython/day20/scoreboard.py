6a1f738f65c0682ba63a1d0b2e4f14425c264489
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-17 22:02:21 +0500

    psh

--- 100DaysofPython/day20/scoreboard.py
+++ 100DaysofPython/day20/scoreboard.py
@@ -16,6 +16,10 @@ class Scoreboard(Turtle):
     def update(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over.", align=ALIGNMENT, font=FONT)
+
     def increase_score(self):
         self.score += 1
         self.clear()
