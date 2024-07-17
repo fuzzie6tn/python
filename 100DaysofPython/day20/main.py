3dd638784ddaff67fd7f05a4b64667cd54ad6c6f
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-17 21:11:37 +0500

    psh

--- 100DaysofPython/day20/main.py
+++ 100DaysofPython/day20/main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')  # background color
@@ -8,6 +10,7 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -20,8 +23,11 @@ game_is_on = True
 while game_is_on:
     screen.update()  # each update moves
     time.sleep(0.1)  # 1 second delay
-
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
 
