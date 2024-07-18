555b21a7957bdb11b2d8840628ee3ad7171daafe
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-07-18 22:35:51 +0500

    psh

--- 100DaysofPython/day20/main.py
+++ 100DaysofPython/day20/main.py
@@ -39,10 +39,8 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect collision with snake
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
     # if head collides with any segment in the tail :
