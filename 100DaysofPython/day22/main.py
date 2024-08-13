96dcec9b2ce72c6dc30aa72eb434fdfdf7a7428f
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-08-13 23:41:43 +0500

    psh

--- 100DaysofPython/day22/main.py
+++ 100DaysofPython/day22/main.py
@@ -1,4 +1,7 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor('black')
@@ -6,39 +9,21 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)  # turns off the animation
 
-r_paddle = Paddle((350,0))
-l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-# paddle = Turtle()
-# paddle.shape("square")
-# paddle.color("white")
-# paddle.shapesize(stretch_wid=5, stretch_len=1)
-# paddle.penup()
-#
-# paddle.goto(350, 0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+ball = Ball()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
-
 while game_is_on:
+    time.sleep(0.1)  # the ball will move with .1 seconds
     screen.update()  # to turn on the game manually when tracer(0)
-
-
-
-
+    ball.move()
 
 screen.exitonclick()
--- 100DaysofPython/day22/paddle.py
+++ 100DaysofPython/day22/paddle.py
@@ -3,12 +3,19 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()  # abilities get from the parent class
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(350, 0)
+        self.goto(position)
 
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
 
