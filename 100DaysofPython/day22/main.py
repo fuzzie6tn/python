91f6e3b3f448f27e14ab388f9f326d4c624fae75
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-08-13 15:09:54 +0500

    psh

--- 100DaysofPython/day22/main.py
+++ 100DaysofPython/day22/main.py
@@ -4,23 +4,38 @@ screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)  # turns off the animation
 
 paddle = Turtle()
 paddle.shape("square")
 paddle.color("white")
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
+
 paddle.goto(350, 0)
+
 
 def go_up():
     new_y = paddle.ycor() + 20
     paddle.goto(paddle.xcor(), new_y)
 
+
 def go_down():
     new_y = paddle.ycor() - 20
     paddle.goto(paddle.xcor(), new_y)
 
+
 screen.listen()
 screen.onkey(go_up, "Up")
 screen.onkey(go_down, "Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()  # to turn on the game manually when tracer(0)
+
+
+
+
+
 screen.exitonclick()
