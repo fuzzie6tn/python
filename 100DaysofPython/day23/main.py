1a37cf1b5caa8cc6fc21042fd3267828963d61a8
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-08-27 18:11:04 +0500

    psh

--- 100DaysofPython/day23/main.py
+++ 100DaysofPython/day23/main.py
@@ -12,22 +12,31 @@ screen.tracer(0) # turned off the tracer
 
 
 player = Player()
-car = CarManager()
+car_manager = CarManager()
 
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
+
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-    car.create_car()
-    car.move_cars()
+    car_manager.create_car()
+    car_manager.move_cars()
 
     # detect collision with the car
 
-    for car in car.all_cars:
-        if car.distance(player) < 20:
+    for car in car_manager.all_cars:
+        if car.distance(player) < 23:
             game_is_on = False
+
+    # detect if at finish line
+    if player.is_at_finish_line():
+        player.go_to_start()
+
+
+screen.exitonclick()
--- 100DaysofPython/day23/player.py
+++ 100DaysofPython/day23/player.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = -2500
+FINISH_LINE_Y = 280
 
 class Player(Turtle):
     def __init__(self):
@@ -10,11 +10,22 @@ class Player(Turtle):
         self.shape('turtle')
         self.penup()
         self.color('white')
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    # def go_down(self):
+    #     self.down()
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
 
 
