b9493611a8f9022320242e0b9f781385c4a68a15
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-08-29 20:34:53 +0500

    psh

--- 100DaysofPython/day25/main.py
+++ 100DaysofPython/day25/main.py
@@ -6,5 +6,8 @@ import csv
 
 with open("weather_data.csv") as data_file:
     data = csv.reader(data_file)
+    temperature = []
     for row in data:
-        print(row)
+       if row[1] != "temp":
+           temperature.append(row[1])
+    print(temperature)
