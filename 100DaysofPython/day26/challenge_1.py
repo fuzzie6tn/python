3eeccc0f44a1f16378f7aaeadd8e7f329950a872
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-01 14:02:08 +0500

    psh

---
+++ 100DaysofPython/day26/challenge_1.py
@@ -0,0 +1,3 @@
+numbers = [1,1,2,3,5,8,13,21,34,55]
+squared_numbers = [n**2 for n in numbers]
+print(squared_numbers)
---
+++ 100DaysofPython/day26/challenge_2.py
@@ -0,0 +1,3 @@
+numbers = [1,1,2,3,5,8,13,21,34,55]
+result = [n for n in numbers if(n)%2==0]
+print(result)
