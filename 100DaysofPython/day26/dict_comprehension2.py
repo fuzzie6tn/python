04a86a1fe42f2172d00362cf220b1e234cb9be5a
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-02 18:26:03 +0500

    psh

---
+++ 100DaysofPython/day26/dict_comprehension2.py
@@ -0,0 +1,11 @@
+weather_c = {
+    "Monday" : 12,
+    "Tuesday" : 13,
+    "Wednesday" : 12,
+    "Thursday" : 14,
+    "Friday" : 15,
+    "Saturday" : 16,
+}
+
+# weather_f = {day, (temp_c*9/5)+ 32 for (day, temp_c) in weather_c.items()}
+# print(weather_f)
