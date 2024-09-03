68d3dc6cff2cd954ac27d10340fdbf37bcb0b694
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-03 19:45:53 +0500

    psh

--- 100DaysofPython/day27/main.py
+++ 100DaysofPython/day27/main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter.ttk import Button
 
 window = tkinter.Tk()
 window.title("Gui")
@@ -9,4 +10,11 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(window, text="Hello World", font=("Calibri", 20, "normal"))
 my_label.pack() # this will automatically bring it to the center
 
+
+def button_clicked():
+    my_label.config(text="Button got clicked")
+    # print("I got clicked")
+
+button = Button(text = "Click Me", command=button_clicked)
+button.pack()
 window.mainloop()
