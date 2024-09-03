18003bbbd7f63b61956baa5b40aee90eb784647f
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-03 11:39:14 +0500

    psh

--- 100DaysofPython/day27/main.py
+++ 100DaysofPython/day27/main.py
@@ -5,7 +5,7 @@ window.title("Gui")
 window.minsize(width=500, height=300)
 
 # label
-my_label = tkinter.Label(window, text="Hello World", font=("Arial", 24, "bold"))
+my_label = tkinter.Label(window, text="Hello World", font=("Calibri", 20, "normal"))
 my_label.pack() # this will automatically bring it to the center
 
 window.mainloop()
