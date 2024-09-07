dc627207fff336bd083196c30c8db2ac95c40bd6
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-07 14:59:36 +0500

    psh

--- 100DaysofPython/day28/main.py
+++ 100DaysofPython/day28/main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+
+from nala import color
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -19,11 +22,21 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+# label
+timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
 
+# Img
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file = "download.png")
 canvas.create_image(100,112, image=tomato_img)
 canvas.create_text(100,133, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"), anchor=CENTER)
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+# Buttons
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
 window.mainloop()
