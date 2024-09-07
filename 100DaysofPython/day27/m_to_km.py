10de9ce5c3a1e3d65e1172eb2c91bc56621e81c1
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-07 13:29:54 +0500

    psh

--- 100DaysofPython/day27/m_to_km.py
+++ 100DaysofPython/day27/m_to_km.py
@@ -1,33 +1,33 @@
 from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Mile to Kilo Mile")
 window.configure(padx=15, pady=15)
 window.minsize(width=150, height=100)
 
 # Entry
-mile = tkinter.Entry(width = 10)
+mile = Entry(width = 10)
 mile.grid(row = 0, column = 1)
 mile.get()
 
 # Label
-text_miles = tkinter.Label(text = "Miles")
+text_miles = Label(text = "Miles")
 text_miles.grid(row = 0, column = 2)
 
-text_2 = tkinter.Label(text = "is equal to")
+text_2 = Label(text = "is equal to")
 text_2.grid(row = 1, column = 0)
 
 text_3 =  0
-text_3 = tkinter.Label(text = f"{text_3}")
+text_3 = Label(text = f"{text_3}")
 text_3.grid(row = 1, column = 1)
 
-text_4 = tkinter.Label(text = "Km")
+text_4 = Label(text = "Km")
 text_4.grid(row = 1, column = 2)
 
 #Button
 def calculate():
 
-button = tkinter.Button(text="Calculate", command=calculate)
+button = Button(text="Calculate", command=calculate)
 
 
 
