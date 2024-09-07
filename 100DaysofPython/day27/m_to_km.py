824c32e48889eb7c87af9b07692e1f2befa24e56
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-07 13:34:04 +0500

    psh

--- 100DaysofPython/day27/m_to_km.py
+++ 100DaysofPython/day27/m_to_km.py
@@ -3,31 +3,30 @@ from tkinter import *
 window = Tk()
 window.title("Mile to Kilo Mile")
 window.configure(padx=15, pady=15)
-window.minsize(width=150, height=100)
+# window.minsize(width=150, height=100)
 
 # Entry
-mile = Entry(width = 10)
-mile.grid(row = 0, column = 1)
-mile.get()
+mile_input = Entry(width = 10)
+mile_input.grid(row = 0, column = 1)
+mile_input.get()
 
 # Label
-text_miles = Label(text = "Miles")
-text_miles.grid(row = 0, column = 2)
+miles_label = Label(text ="Miles")
+miles_label.grid(row = 0, column = 2)
 
-text_2 = Label(text = "is equal to")
-text_2.grid(row = 1, column = 0)
+is_equal_label = Label(text ="is equal to")
+is_equal_label.grid(row = 1, column = 0)
 
-text_3 =  0
-text_3 = Label(text = f"{text_3}")
-text_3.grid(row = 1, column = 1)
+kilometer_result_label = Label(text = "0")
+kilometer_result_label.grid(row = 1, column = 1)
 
-text_4 = Label(text = "Km")
-text_4.grid(row = 1, column = 2)
+kilometer_label = Label(text ="Km")
+kilometer_label.grid(row = 1, column = 2)
 
 #Button
 def calculate():
 
-button = Button(text="Calculate", command=calculate)
+calculate_button = Button(text="Calculate", command=calculate)
 
 
 
