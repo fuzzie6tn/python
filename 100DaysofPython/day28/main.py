7021748f33a29e54a5b9026e73226b1a65575022
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-11 17:57:05 +0500

    psh

--- 100DaysofPython/day28/main.py
+++ 100DaysofPython/day28/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import math
 
+from click import command
 from docutils.nodes import title_reference
 from nala import color
 
@@ -15,6 +16,7 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 timer = ""
+paused = False
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
     window.after_cancel(timer)
@@ -22,10 +24,18 @@ def reset_timer():
     # 00:00 timer_text
     # title "Timer"
     canvas.itemconfig(timer_text, text="00:00")
-    timer_label.config(text="Timer")
+    title_label.config(text="Timer")
     check_mark_label.config(text="")
+    start_button.config(text="Start")
     global reps
     reps = 0
+# ---------------------------- Stop MECHANISM ------------------------------- #
+def pause_timer():
+    global paused
+    if paused == True:
+        # window.after_cancel(timer)
+        start_button.config(text="Pause")
+        title_label.config(text="Paused", fg="Blue")
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
@@ -37,17 +47,18 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
-        timer_label.config(text="Break", fg=RED)
+        title_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
-        timer_label.config(text="Break", fg=PINK)
+        title_label.config(text="Break", fg=PINK)
     else:
         count_down(WORK_MIN * 60)
-        timer_label.config(text="Work", fg=GREEN)
+        title_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    global paused
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
@@ -71,8 +82,8 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 # label
-timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
-timer_label.grid(column=1, row=0)
+title_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+title_label.grid(column=1, row=0)
 
 # Img
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -82,7 +93,7 @@ timer_text = canvas.create_text(100,133, text="00:00", fill="white", font=(FONT_
 canvas.grid(column=1, row=1)
 
 # Buttons
-start_button = Button(text="Start", bg="white", command=start_timer, highlightthickness=0, borderwidth=0)
+start_button = Button(text="Start", bg="white", command=start_timer,highlightthickness=0, borderwidth=0)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset",bg="white",highlightthickness=0, borderwidth=0, command=reset_timer)
