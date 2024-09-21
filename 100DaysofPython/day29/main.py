23cb0b764c8d3675a6fef21f5e8f4e4e4410c77d
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-21 13:49:12 +0500

    psh

--- 100DaysofPython/day29/main.py
+++ 100DaysofPython/day29/main.py
@@ -11,9 +11,9 @@ window.config(padx=20, pady=20, bg="White")
 window.geometry("500x500")
 # Image
 
-canvas = Canvas(window, width=400, height=400)
+canvas = Canvas(width=200, height=200, bg="White", highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(200,200, image=logo_img)
+canvas.create_image(100,100, image=logo_img)
 
 canvas.pack()
 window.mainloop()
