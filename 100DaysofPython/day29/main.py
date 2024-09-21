34a7f1b5131bb3bbbe215549697cbbed48c57288
Fazila Roshan <fazilaroushanbeg@gmail.com>
2024-09-21 15:50:32 +0500

    psh

--- 100DaysofPython/day29/main.py
+++ 100DaysofPython/day29/main.py
@@ -3,49 +3,48 @@ from tkinter import *
 # ------------ Password generate -------------------- #
 
 # ----------- Save Password ------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = pass_entry.get()
 
-# --------- UI setup -------------------------------- #
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+
+# Create window
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20, bg="White")
+window.config(padx=50, pady=50, bg="white")
 
-# Image
-canvas = Canvas(width=200, height=200, bg="White", highlightthickness=0)
+# Canvas for logo
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100,100, image=logo_img)
+canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
-# Website label
-website_label = Label(text="Website", bg="White")
+# Labels
+website_label = Label(text="Website:", bg="white")
 website_label.grid(column=0, row=1)
-
-# Website entry
-website_entry = Entry(width=35)
-website_entry.grid(columnspan=2, column=1,row=1)
-
-# Email-username label
-email_label = Label(text="Email/Username", bg="White")
+email_label = Label(text="Email/Username:", bg="white")
 email_label.grid(column=0, row=2)
-
-# Email entry
-email_entry = Entry(width=35)
-email_entry.grid(columnspan=2, column=1,row=2)
-
-# Password label
-pass_label = Label(text="Password", bg="White")
+pass_label = Label(text="Password:", bg="white")
 pass_label.grid(column=0, row=3)
 
-# Password entry
-pass_entry = Entry(width=21)
-pass_entry.grid(column=1,row=3)
+# Entries
+website_entry = Entry(width=40)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=40)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "fazi@gmail.com")
+pass_entry = Entry(width=28)
+pass_entry.grid(column=1, row=3)
 
-# Generate Password Button
-generate_button = Button(text="Generate Password", bg="White")
-generate_button.grid(column=2, row=3)
+# Buttons
+generate_button = Button(text="Generate Password", bg="white",width=15)
+generate_button.grid(column=2, row=3,padx=(0, 10))
 
-# Add Button
-add_button = Button(text="Add", bg="White", width=36)
-add_button.grid(columnspan = 2,column=1, row=4)
-
+add_button = Button(text="Add", width=41, bg="white",command=save)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
