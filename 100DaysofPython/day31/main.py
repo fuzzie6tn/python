83438f4472357499315f32ae552653839c08072d
fuzzie@6tn <fazilaroushanbeg@gmail.com>
2024-10-05 15:53:12 +0500

    push

---
+++ 100DaysofPython/day31/main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800,height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 30, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 35, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image)
+unknown_button.grid(row=1,column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image)
+known_button.grid(row=1,column=1)
+
+window.mainloop()
