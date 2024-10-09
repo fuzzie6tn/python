0e56f9673e7c90ce13e0ac7ac32af35dafdc541c
fuzzie@6tn <fazilaroushanbeg@gmail.com>
2024-10-09 15:55:06 +0500

    psuh

--- 100DaysofPython/day32/main.py
+++ 100DaysofPython/day32/main.py
@@ -1,12 +1,21 @@
-import smtplib
+# import smtplib
+#
+# my_email = "fazilaroushanbeg@gmail.com"
+# password = "trlpvnxratypoxap"
+# #   trlp vnxr atyp oxap
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls() # tls - transport layer security (makes it secure) way of securing our connection to our email server
+#     # in simple words it encrypts the message and makes it impossible to read what's in our content of email
+#     connection.login(user=my_email,password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="optimist.alishan@gmail.com",
+#                         msg="Subject:Sending with subject\n\n i am fish, i am sending this message with python code")
 
-my_email = "fazilaroushanbeg@gmail.com"
-password = "trlpvnxratypoxap"
-#   trlp vnxr atyp oxap
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls() # tls - transport layer security (makes it secure) way of securing our connection to our email server
-    # in simple words it encrypts the message and makes it impossible to read what's in our content of email
-    connection.login(user=my_email,password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="optimist.alishan@gmail.com",
-                        msg="Subject:Sending with subject\n\n i am fish, i am sending this message with python code")
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+week_day = now.weekday()
+print(week_day)
+
