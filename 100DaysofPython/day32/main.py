21a5b6519c54ec4de9989c13323d0bf490bad09d
fuzzie@6tn <fazilaroushanbeg@gmail.com>
2024-10-09 16:46:56 +0500

    psuh

--- 100DaysofPython/day32/main.py
+++ 100DaysofPython/day32/main.py
@@ -11,11 +11,32 @@
 #                         to_addrs="optimist.alishan@gmail.com",
 #                         msg="Subject:Sending with subject\n\n i am fish, i am sending this message with python code")
 
+# now = dt.datetime.now()
+# year = now.year
+# week_day = now.weekday()
+# print(week_day)
 
+
+import smtplib
 import datetime as dt
+import random
+
+my_email = "fazilaroushanbeg@gmail.com"
+password = "trlpvnxratypoxap"
 
 now = dt.datetime.now()
-year = now.year
 week_day = now.weekday()
-print(week_day)
+
+if week_day == 3: # Monday
+    with open("quotes.txt", "r") as quote_file:
+        quotes = quote_file.readlines()
+        quote = random.choice(quotes)
+
+    print(quote)
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,to_addrs=my_email,msg=f"Subject: Quote of the day\n\nSending you a random quote from my list of quotes\n{quote}")
+
 
