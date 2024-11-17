efdbd8cca5379f6fb4631da8fa24cfeb6f6d7703
Fazila <fazilaroushanbeg@gmail.com>
2024-11-17 22:29:05 +0500

    oush

--- 100DaysofPython/day33/main.py
+++ 100DaysofPython/day33/main.py
@@ -1,7 +1,11 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-if response.status_code == 404:
-    raise Exception("That resourse does not exist")
-elif response.status_code == 401:
-    raise Exception("You are not authorized to access this resource")
+# if response.status_code == 404:
+#     raise Exception("That resourse does not exist")
+# elif response.status_code == 401:
+#     raise Exception("You are not authorized to access this resource")
+response.raise_for_status()
+data = response.json()
+
+print(data)
