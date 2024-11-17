93793192af61fecb6b8eede5e8a8ffbc8b30b5cb
Fazila <fazilaroushanbeg@gmail.com>
2024-11-17 22:31:42 +0500

    oush

--- 100DaysofPython/day33/main.py
+++ 100DaysofPython/day33/main.py
@@ -6,6 +6,6 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # elif response.status_code == 401:
 #     raise Exception("You are not authorized to access this resource")
 response.raise_for_status()
-data = response.json()
+data = response.json()["iss_position"]
 
 print(data)
