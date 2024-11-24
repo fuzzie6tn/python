1fd9f626374225fb62f1bf2268f37ba87f8a4816
Fazila <fazilaroushanbeg@gmail.com>
2024-11-24 16:53:46 +0500

    push

--- 100DaysofPython/day33/main.py
+++ 100DaysofPython/day33/main.py
@@ -27,11 +27,11 @@ parameters = {
 response = requests.get("https://api.sunrise-sunset.org/json" , params = parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
-print(sunrise.split('T'))
+sunrise = data["results"]["sunrise"].split('T')[1].split(":")[0]
+sunset = data["results"]["sunset"].split('T')[1].split(":")[0]
+
 time_now = datetime.now()
 
-print(sunrise)
+# print(sunrise)
 
-print(time_now)
+# print(time_now)
