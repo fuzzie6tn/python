9066173e11b732be06236d27fec2965d3780696f
Fazila <fazilaroushanbeg@gmail.com>
2024-11-24 16:37:27 +0500

    push

--- 100DaysofPython/day33/main.py
+++ 100DaysofPython/day33/main.py
@@ -1,13 +1,30 @@
+# import requests
+#
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # if response.status_code == 404:
+# #     raise Exception("That resourse does not exist")
+# # elif response.status_code == 401:
+# #     raise Exception("You are not authorized to access this resource")
+# response.raise_for_status()
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (latitude, longitude)
+# print(iss_position)
+
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# if response.status_code == 404:
-#     raise Exception("That resourse does not exist")
-# elif response.status_code == 401:
-#     raise Exception("You are not authorized to access this resource")
+MY_LAT = 30.375320
+MY_LONG = 69.345116
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json" , params = parameters)
 response.raise_for_status()
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position = (latitude, longitude)
-print(iss_position)
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(sunrise)
