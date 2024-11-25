d1315ad8654db3b89857f258b6f91da6d5a6eded
Fazila <fazilaroushanbeg@gmail.com>
2024-11-26 01:40:41 +0500

    push

--- 100DaysofPython/day34/002 quizzler-app-start/ui.py
+++ 100DaysofPython/day34/002 quizzler-app-start/ui.py
@@ -1,2 +1,32 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0",bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0,column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg= "white")
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     text="Some Question",
+                                                     fill=THEME_COLOR,
+                                                     font= ("Arial", 20, "italic")
+                                                     )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=1)
+
+
+        self.window.mainloop()
