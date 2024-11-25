d7da2381e8fdbb6e3c724f1968207beb12f2d298
Fazila <fazilaroushanbeg@gmail.com>
2024-11-26 00:39:58 +0500

    push

--- 100DaysofPython/day34/002 quizzler-app-start/data.py
+++ 100DaysofPython/day34/002 quizzler-app-start/data.py
@@ -1,15 +1,13 @@
 import requests
 
 parameters = {
-    "amount":10,
-     "type:": "boolean"
+    "amount": 10,
+     "type": "boolean"
 }
 response = requests.get ("https://opentdb.com/api.php" , params=parameters)
-
-
-
-
-
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
 
 
 # question_data = [
