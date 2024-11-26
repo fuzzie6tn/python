730668b401d91510a6de259c5d6558c33562eab6
Fazila <fazilaroushanbeg@gmail.com>
2024-11-27 01:21:43 +0500

    push

--- 100DaysofPython/day34/002 quizzler-app-start/ui.py
+++ 100DaysofPython/day34/002 quizzler-app-start/ui.py
@@ -38,10 +38,25 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
 
     def true_pressed(self):
-        pass
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
