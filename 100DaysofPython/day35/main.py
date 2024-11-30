e1f00084514effa2f4025eb3558e10673bf8cc79
Fazila <fazilaroushanbeg@gmail.com>
2024-11-30 14:47:19 +0500

    push

--- 100DaysofPython/day35/main.py
+++ 100DaysofPython/day35/main.py
@@ -1,20 +1,20 @@
 # rain alert
 import requests
 
-OWM_endpoint = "https://api.openweathermap.org/data/3.0/onecall"
-api_key = "7dfde1fd0c46b729c827bd2db6c326f0"
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "3fc8982ac5d3a42b86c1b7b98927dd8a"
 
 weather_params = {
     "lat": 30.375320,
     "lon": 69.345116,
     "appid": api_key,
+    "exclude": "current,minutely,daily",
 }
 
 response = requests.get(OWM_endpoint, params=weather_params)
 response.raise_for_status()
-hourly_data = response.json()
-print(hourly_data['hourly'])
-
+weather_data = response.json()
+print(weather_data)
 
 
 
