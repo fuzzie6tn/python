1c3b7b4d7bfb1df64cf07a8156be1d22d1ee561b
Fazila <fazilaroushanbeg@gmail.com>
2024-12-05 22:59:11 +0500

    push

--- My_Projects/todoapp.py
+++ My_Projects/todoapp.py
@@ -1,6 +1,18 @@
 from tkinter import *
 from tkinter import font
 
+def add_task():
+    pass
+
+def delete_task():
+    pass
+
+def delete_all_task():
+    pass
+
+def exit_task():
+    pass
+
 window = Tk()
 window.title("To-Do list")
 window.config(padx=50, pady=50, bg="pink")
@@ -9,8 +21,9 @@ window.geometry("600x600")
 fonts = font.families()
 custom_font = font.Font(family="FreeMono", size=45)
 
+
 # Title
-title = Label(text="The TODO List", fg="brown", bg="pink", font=(custom_font))
+title = Label(text="The TODO List", fg="brown", bg="pink", font=custom_font)
 title.config(pady=50)
 title.grid(column=0, row=0, columnspan=3)  # Span across all columns
 
@@ -24,15 +37,15 @@ input_task.grid(column=0, row=2, pady=10, sticky="w")
 
 # Buttons
 add_button = Button(text="Add Task", width=20)
-add_button.grid(column=0, row=3, pady=10, sticky="w")
+add_button.grid(column=0, row=3, pady=10, sticky="w", command=add_task)
 
-delete_button = Button(text="Delete Task", width=20)
+delete_button = Button(text="Delete Task", width=20, command=delete_task)
 delete_button.grid(column=0, row=4, pady=10, sticky="w")
 
-delete_all = Button(text="Delete All", width=20)
+delete_all = Button(text="Delete All", width=20,command=delete_all_task)
 delete_all.grid(column=0, row=5, pady=10, sticky="w")
 
-exit_button = Button(text="Exit", width=20)
+exit_button = Button(text="Exit", width=20, command=exit_task)
 exit_button.grid(column=0, row=6, pady=10, sticky="w")
 
 # Listbox
