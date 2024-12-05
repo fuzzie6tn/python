2cc17eb82d14a99a3781bbbada1cd8830194c502
Fazila <fazilaroushanbeg@gmail.com>
2024-12-05 17:18:18 +0500

    push

--- 100DaysofPython/day35/main.py
+++ 100DaysofPython/day35/main.py
@@ -1,8 +1,12 @@
 # rain alert
 import requests
+from twilio.rest import Client
+
 
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "3fc8982ac5d3a42b86c1b7b98927dd8a"
+auth_id = "AC374d00f3d9b65735e032c4c4f99c6e81"
+auth_token = "ca0f8b2735e7bf68a23ae2f172422d23"
 
 weather_params = {
     "lat": 30.375320,
@@ -24,10 +28,14 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain:
-    print("Bring and umbrella")
-
-
-# print(weather_data["hourly"][0]["weather"][0]["id"])
+    client = Client(auth_id, auth_token)
+    message = client.messages \
+         .create(
+        body = "Bring an umbrella, it's going to rain today",
+        from = "+18777804236",
+        to = "=923435952375"
+    )
+    print(message.status)
 
 
 
