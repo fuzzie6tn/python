055cf7620c11131f45406a46624ff677a83fa10a
Fazila <fazilaroushanbeg@gmail.com>
2024-12-08 21:50:47 +0500

    push

---
+++ 100DaysofPython/day37/main.py
@@ -0,0 +1,14 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params= {
+    "token": "hahfiuwe7373s33",
+    "username": "fazilaroshan",
+    "agreeTermsOfService": "yes",
+    "notMinor":"yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
