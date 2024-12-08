635ca5c2f1ce36c552857e8f78d523221210177a
Fazila <fazilaroushanbeg@gmail.com>
2024-12-08 17:14:17 +0500

    push

--- 100DaysofPython/day36/main.py
+++ 100DaysofPython/day36/main.py
@@ -1,5 +1,29 @@
+import requests
+import pandas
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "26W0BL08FVG75CFF"
+
+# TODO 1. get yesterday's stocking price
+# alphavantage is a stock market API
+stock_params = {
+    "function" : "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+response = requests.get(STOCK_ENDPOINT, params=stock_params) # fetching from the stock endpoint
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key,value) in data.items()] # list comprehension
+yestarday_data = data_list[0]
+yestarday_closing_price = yestarday_data["4. close"]
+print(yestarday_closing_price)
+
+# TODO 2. Get the day before yesterday's closing stock price
+before_closing_price = data_list[1]
+day_before_yesterday = before_closing_price["4. close"]
+print(day_before_yesterday)
