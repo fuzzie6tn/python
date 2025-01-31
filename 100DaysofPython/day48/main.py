f9e73602669e6926dcfc639d9f5d7a68f136bcf1
Fazila <fazilaroushanbeg@gmail.com>
2025-01-31 16:49:42 +0500

    push

--- 100DaysofPython/day48/main.py
+++ 100DaysofPython/day48/main.py
@@ -10,6 +10,18 @@ driver.get("https://www.python.org/")
 # search_bar =  driver.find_element_by_name("q")
 # print(search_bar.tag_name("placeholder"))
 
+event_times = driver.find_elements_by_css_selector(".event-widget time")
+# for time in event_times:
+#     print(time.text)
+event_name = driver.find_elements_by_css_selector(".event-widget li a")
+for name in event_name:
+    print(name.text)
 
+events = {}
+for n in range(len(event_times)):
+    events[n]={
+        "time": event_times[n],
+        "name":event_name[n]
+    }
 
 driver.quit()
