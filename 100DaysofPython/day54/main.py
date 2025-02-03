78ec1bfd8dee2a58a8fb3f3601bcf950ac86b492
Fazila <fazilaroushanbeg@gmail.com>
2025-02-03 11:26:08 +0500

    flask

--- 100DaysofPython/day54/main.py
+++ 100DaysofPython/day54/main.py
@@ -1,7 +1,14 @@
 # Flask
 from flask import Flask
+import random
+
 app = Flask(__name__)
+print(random.__name__)
+print(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World'
+
+if __name__ == "__main__":
+    app.run()
