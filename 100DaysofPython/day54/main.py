e5098ef7932901bfd742334de88b3a330b6c9479
Fazila <fazilaroushanbeg@gmail.com>
2025-02-05 14:53:59 +0500

    flask

--- 100DaysofPython/day54/main.py
+++ 100DaysofPython/day54/main.py
@@ -8,10 +8,21 @@ print(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World'
+    return '<h1 style="text-align:center">Hello world</h1>'\
+            '<p>this is a pargraph</p>'\
+            '<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGowdnFpdjZ3ODdoa2QwNGwzYWJ2ZjBiM3kxaTZia2k4dGVrenlkbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/OmK8lulOMQ9XO/giphy.gif">'
+
+@app.route('/bye')
+def bye():
+    return "bye"
+
+@app.route('/username/<name>/<int:number>')
+def username(name, number):
+    # return f"Hello {name}, you are {number} years old!"
+    pass
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
 # python decorator function
